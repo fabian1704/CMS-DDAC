6cada24997662d38c4359088504a5d162ff30cc0
och95 <ongch1995@gmail.com>
2019-05-04 13:09:53 +0800

    css fix-2

--- CMS/App_Start/BundleConfig.cs
+++ CMS/App_Start/BundleConfig.cs
@@ -26,7 +26,6 @@ namespace CMS
                         "~/Scripts/modernizr-*"));
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
-                        "~/Content/bootstrap.css",
                         "~/Content/site.css"
                        ));
         }
